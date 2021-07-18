f5addb89a1029de26efd66549d33d412f6527500
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-07-18 12:32:52 +0100

    Added Airport model and added airports to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Seeding Airports...'
+
+Airport.create(name: 'Aberdeen', airport_code: 'ABZ')
+Airport.create(name: 'Edinburgh', airport_code: 'EDI')
+Airport.create(name: 'Glasgow', airport_code: 'GLA')
+Airport.create(name: 'London Gatwick', airport_code: 'LGW')
+Airport.create(name: 'London Stanstead', airport_code: 'STN')
+Airport.create(name: 'London Heathrow', airport_code: 'LHR')
+Airport.create(name: 'Melbourne', airport_code: 'MEL')
+Airport.create(name: 'New York', airport_code: 'NYC')
+Airport.create(name: 'San Francisco', airport_code: 'SFO')
+Airport.create(name: 'Colombia', airport_code: 'CLO')
+
+puts 'Finished Seeding Airports!'
+
+
