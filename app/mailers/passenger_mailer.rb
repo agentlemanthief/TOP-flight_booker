10beb3b4dfb5a418b4ccb0f099009b8cc473779b
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-08-09 11:20:04 +0100

    Working PassengerMailer in development.

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,8 +1,8 @@
 class PassengerMailer < ApplicationMailer
   def thank_you_email
     @passenger = params[:passenger]
-    @url = "http://127.0.0.1/bookings/#{params[:id]}"
+    @url = "http://127.0.0.1:3000/bookings/#{params[:id]}"
 
-    mail(to: passenger.email, subject: 'Your Flight Booking')
+    mail(to: @passenger.email, subject: 'Your Flight Booking')
   end
 end
