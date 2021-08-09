6b100886c791601836c878a8f0fc8e668267ed69
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-08-09 11:59:28 +0100

    Added config for production mailer.

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,7 +1,11 @@
 class PassengerMailer < ApplicationMailer
   def thank_you_email
     @passenger = params[:passenger]
-    @url = "http://127.0.0.1:3000/bookings/#{params[:id]}"
+    # @url for development
+    # @url = "http://127.0.0.1:3000/bookings/#{params[:id]}"
+
+    # @url for production
+    @url = "https://pacific-tundra-92135.herokuapp.com/bookings/#{params[:id]}"
 
     mail(to: @passenger.email, subject: 'Your Flight Booking')
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
+  # Gmail config for production mailer testing
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'example.com',
+  #   user_name:            ENV['GMAIL_USER'],
+  #   password:             ENV['GMAIL_PASS'],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
