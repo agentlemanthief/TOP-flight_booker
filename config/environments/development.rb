c84d4a6edf31b1d4d5db5901ddcd0d45fd0f8876
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-08-09 10:16:26 +0100

    Created #thank_you_email action in PassengerMailer.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Cofig for Letter Opener gem
+  # Config for Letter Opener gem
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
